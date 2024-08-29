e08e73dfbadf24e83cc3316aafb8caadbea9e249
belaletech <belalahmad@lambdatest.com>
2024-08-29 15:59:09 +0530

    update

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -11,32 +11,39 @@ public class AppTest {
     static AndroidDriver driver;
 
     public static void main(String[] args) throws MalformedURLException {
-        openMobileApp();
+        AndroidApp1("Galaxy.*", "11", "Android");
         testing();
     }
 
-    public static void openMobileApp() throws MalformedURLException {
-        try {
-            DesiredCapabilities cap = new DesiredCapabilities();
-            cap.setCapability("deviceName", "mediumFirst");
-            cap.setCapability("platformName", "Android");
-            cap.setCapability("platformVersion", "7.0");
-            cap.setCapability("automationName", "UiAutomator2");
-            cap.setCapability("app", "C:\\Users\\belalahmad\\Desktop\\appiumProject\\frist_android_test\\src\\main\\resources\\app-release.apk");
-            cap.setCapability("noReset", true);
+    public static void AndroidApp1(String device, String version, String platform) throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("build", "Java TestNG");
+        capabilities.setCapability("name", platform + " " + device + " " + version);
+        capabilities.setCapability("deviceName", device);
+        capabilities.setCapability("platformVersion", version);
+        capabilities.setCapability("platformName", platform);
+        capabilities.setCapability("isRealMobile", true);
+        capabilities.setCapability("app", "lt://APP10160571441724918016368480");
+        capabilities.setCapability("deviceOrientation", "PORTRAIT");
+        capabilities.setCapability("network", false);
+        capabilities.setCapability("visual", true);
+        capabilities.setCapability("devicelog", true);
+        capabilities.setCapability("autoGrantPermissions", true);
 
-            URL url = new URL("http://127.0.0.1:4723/wd/hub");
-            driver = new AndroidDriver(url, cap);
-            System.out.println("Application started");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        String userName = "belalahmad";
+        String accessKey = "cousQqH3syuMR3H55LiQfG4QqCyPHRsZs3XJ3mbEle94hOdYLj";
+        String gridUrl = "mobile-hub.lambdatest.com";
+
+        // Use AndroidDriver directly without casting
+        driver = new AndroidDriver(new URL("https://" + userName + ":" + accessKey + "@" + gridUrl + "/wd/hub"), capabilities);
     }
 
     public static void testing() {
-        // Set the timeout in seconds directly
+        // Your testing logic here
         WebDriverWait wait = new WebDriverWait(driver, 10);
-//        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("Subtract"))).click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("Add"))).click();
+        for (int i = 0; i < 10; i++) { // Loop to perform the click action 10 times
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.id("Add"))).click();
+            System.out.println("Click " + (i + 1) + " performed.");
+        }
     }
 }
